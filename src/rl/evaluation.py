ccda2a6239c29e06834e88a89691d50d45f5c4ea
juanjo3ns <juanjo.3ns@gmail.com>
2019-05-25 23:49:06 +0100

    New and final evaluation from epoch 50 to 200

--- src/rl/evaluation.py
+++ src/rl/evaluation.py
@@ -45,35 +45,39 @@ for it in iterations:
 	Q = Q.type(dtype)
 
 	num_episodes = 100
-	save_episodes = 1
+	save_episodes = 2
 	lost = 0
 	mvs = []
 	rwr = []
-	total_coords = []
 
 	for i in range(num_episodes):
 		initState = board.resetInitRandomly()
-		total_coords.append([initState,board.terminalState])
 		done = False
 		while not done:
 			# board.printBoard(initState)
 			action = eval_step(Q,initState)
 			reward, nextState, done = board.takeAction(initState, action)
-			if save_episodes > i:
-				total_coords.append([nextState,board.terminalState])
 			initState = nextState
 			if board.movements > board.maxSteps:
 				lost += 1
 				break
 		mvs.append(board.movements)
 		rwr.append(board.totalreward)
-
 	avg_mvs = sum(mvs)/num_episodes
 	avg_rwr = sum(rwr)/num_episodes
 	message = "ITERATION: {}\nVICTORIES: {}\nDEFEATS: {}\nAVERAGE REWARD: {}\nAVERAGE MOVEMENTS: {}".format(it,(num_episodes-lost), lost, avg_rwr, avg_mvs)
 	print(message)
-
 	csv.write([it.split('.')[0],(num_episodes-lost)/num_episodes*100],[round(avg_rwr,2),avg_mvs])
-	for st in total_coords:
-		csv.write(st[0],st[1])
+
+	for i in range(save_episodes):
+		initState = board.resetInitRandomly()
+		csv.write(initState,board.terminalState)
+		done = False
+		while not done:
+			action = eval_step(Q,initState)
+			reward, nextState, done = board.takeAction(initState, action)
+			csv.write(nextState,board.terminalState)
+			initState = nextState
+			if board.movements > board.maxSteps:
+				break
 	csv.close()
